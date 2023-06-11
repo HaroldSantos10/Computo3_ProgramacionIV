033d7cab478f56814f6a4710d0fd02135058347a
HaroldSantos10 <haroldsan9.9@gmail.com>
2023-06-11 08:50:48 -0600

    Api consumo con Vuejs

--- applaravelsem19/resources/views/pet/create.blade.php
+++
@@ -1,6 +0,0 @@
-Nuevo Registro de Mascota
-
-<form action="{{ url('/pet') }}" method="POST">
-    @csrf
-    @include('pet.form');
-</form>
--- applaravelsem19/resources/views/pet/edit.blade.php
+++
@@ -1,7 +0,0 @@
-Editar Registro de Mascota
-
-<form action="{{ url('/pet7'.$pet->id) }}" method="POST">
-    @csrf
-    {{ method_field('PUT') }}
-    @include('pet.form');
-</form>
